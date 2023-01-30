362e7647cf249970884435c6c9e158bbc6c1aff2
AndreySamojlov <samojlovandrey@gmail.com>
2023-01-30 13:30:49 +0300

    My first commit

---
+++ hello.py
@@ -0,0 +1,8 @@
+user_name = input('What is your name, dude?')
+greeting = f'Hello {user_name}. Glad to see you!'
+print(greeting)
+user_age = int(input(f'How old are you, {user_name}?'))
+birth_year = 2023-user_age
+a ='this century'
+if birth_year < 2000 : a='last century'
+print(f'It means you born in {a}!')
